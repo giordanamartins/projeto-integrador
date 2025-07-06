7358c87ed67996043db34c69ae4c0767b799b0b1
Giordana K. Martins <130246793+giordanamartins@users.noreply.github.com>
2025-07-06 01:47:47 -0300

    update: contas a pagar routes

--- app/backend/src/routes/cpagarRoutes.js
+++ app/backend/src/routes/cpagarRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-
 const cpagarController = require('../controllers/cpagarController');
-
 
 router.get('/', cpagarController.getContasPagar);
 router.get('/hoje', cpagarController.getContasPagarHoje);
 
+router.post('/', cpagarController.createContasPagar);
+
+router.put('/:id', cpagarController.updateContasPagar);
+
+router.delete('/', cpagarController.deleteContasPagar);
 
 module.exports = router;
