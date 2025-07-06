4a879f11d1465aaaae953f040e71a84cc2e34729
Giordana K. Martins <130246793+giordanamartins@users.noreply.github.com>
2025-07-06 01:48:16 -0300

    update: contas a receber routes

--- app/backend/src/routes/creceberRoutes.js
+++ app/backend/src/routes/creceberRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-
 const creceberController = require('../controllers/creceberController');
 
 router.get('/', creceberController.getContasReceber);
 router.get('/hoje', creceberController.getContasReceberHoje);
 
+router.post('/', creceberController.createContasReceber);
+
+router.put('/', creceberController.updateContasReceber);
+
+router.delete('/', creceberController.deleteContasReceber);
 
 module.exports = router;
