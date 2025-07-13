0bab90269a74c93354a8e0a23595a8eb3ede33ce
Giordana K. Martins <130246793+giordanamartins@users.noreply.github.com>
2025-07-13 00:30:53 -0300

    update: remove script sidebar

--- app/frontend/src/pages/categoriaDespesas/novaCatD.js
+++ app/frontend/src/pages/categoriaDespesas/novaCatD.js
@@ -1,21 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const financeiroToggle = document.getElementById('financeiro-toggle');
-    const financeiroSubmenu = document.getElementById('financeiro-submenu');
-    const financeiroArrow = document.getElementById('financeiro-arrow');
-    if (financeiroToggle) {
-        financeiroToggle.addEventListener('click', (event) => {
-            // Impede o comportamento padrão do link (que seria navegar para '#')
-            event.preventDefault();
-
-            // Alterna a classe 'hidden' no submenu, fazendo-o aparecer ou desaparecer
-            financeiroSubmenu.classList.toggle('hidden');
-
-            // Alterna a classe 'rotate-180' no ícone da seta para girá-lo
-            financeiroArrow.classList.toggle('rotate-180');
-        });
-    }
-});
-
 const apiUrl = '/api/catDespesas';
 const form = document.getElementById('form_categoria');
 
