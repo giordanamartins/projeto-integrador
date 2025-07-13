404196ab593fa25c9bb78c4ba77334505d57f7d9
Giordana K. Martins <giordana2004@gmail.com>
2025-07-13 17:56:26 -0300

    update: update & delete usuários

--- app/backend/src/controllers/usuarioController.js
+++ app/backend/src/controllers/usuarioController.js
@@ -10,7 +10,7 @@ const createUser = async (req, res) => {
         return res.status(400).json({ message: 'Nome, email, senha e tipo de usuário são obrigatórios.' });
     }
 
-     if (tipo_usuario === 'advogado' && !numero_oab) {
+    if (tipo_usuario === 'advogado' && !numero_oab) {
         return res.status(400).json({ message: 'O número da OAB é obrigatório para o tipo de usuário "advogado".' });
     }
 
@@ -44,7 +44,56 @@ const getAdvogados = async (req, res) => {
     }
 };
 
+const updateUsuario = async (req, res) => {
+    const { id } = req.params;
+    
+    const usuarioExiste = await db.oneOrNone("SELECT id FROM usuarios WHERE codigo = $1;", [id]);
+    if (!usuarioExiste) {
+        return res.status(404).send({ error: 'Usuário não encontrada.' });
+    }
+    
+    try {
+        const { nome, email, senha, tipo_usuario, numero_oab } = req.body;
+
+        await db.none(
+        'UPDATE usuarios SET nome = $1, email = $2, senha = $3, tipo_usuario = $4, numero_oab = $5 WHERE codigo = $6;'
+        [nome, email, senha, tipo_usuario, numero_oab, id]
+        );
+
+        return res.status(200).send("Usuário atualizado com sucesso!");
+    } 
+    catch (err) {
+        return res.status(500).send({ error: 'Erro ao atualizar usuário.', details: err.message });
+    }
+}
+
+const deleteUsuario = async (req, res) => {
+    const {ids} = req.body;
+    
+    if (!ids || ids.length === 0) {
+        return res.status(400).json({ message: 'Nenhum ID de usuário foi fornecido para exclusão.' });
+    }
+    const idInt = ids.map(id => parseInt(id, 10));
+    
+    console.log("Array convertido para inteiros:", idInt);
+    
+    try {
+        const queryText = 'DELETE FROM usuarios WHERE codigo = ANY($1::int[])';
+
+        const result = await db.query(queryText, [idInt]);
+    
+        // result.rowCount contém o número de linhas que foram deletadas
+        res.status(200).json({ message: `${result.rowCount} usuário(s) excluído(s) com sucesso.` });
+        } 
+    catch (error) {
+        console.error('Erro ao excluir usuário:', error);
+        res.status(500).json({ message: 'Erro interno do servidor.' });
+    }
+}
+
 module.exports = {
     createUser,
-    getAdvogados
+    getAdvogados,
+    updateUsuario,
+    deleteUsuario
 };
